6beb65353fbe13a553ee3cbd299f69eede447926
Kostya <kostyaurysov@gmail.com>
2017-02-28 11:01:39 +0300

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-docker build -t [your_docker_name]/sample-node .
-docker push [your_docker_name]/sample-node
+docker build -t kostyaurysov/sample-node .
+docker push kostyaurysov/sample-node
 
-ssh [gcp_username]@[server_ip_address] << EOF
-docker pull [your_docker_name]/sample-node:latest
+ssh -i deploy deploy@35.187.30.81 << EOF
+docker pull kostyaurysov/sample-node:latest
 # stop and remove a container
 # '|| true' - allows us to ignore any error during the execution of command and proceed
 docker stop web || true
 docker rm web || true
 # remove all images
-docker rmi [your_docker_name]/sample-node:current || true
+docker rmi kostyaurysov/sample-node:current || true
 # change tag of a container from latest to current
-docker tag [your_docker_name]/sample-node:latest [your_docker_name]/sample-node:current
+docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
 # run a container with expost 80 port, which will restart in case of any error
-docker run -d --restart always --name web -p 80:80 [your_docker_name]/sample-node:current
+docker run -d --restart always --name web -p 80:80 kostyaurysov/sample-node:current
 EOF
